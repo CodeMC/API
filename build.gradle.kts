5e1014ae2a3e8ebf990a647ad2f3660b7a2d1fe2
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2024-07-24 20:32:01 -0400

    Upgrade to JDK 17

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,14 +30,14 @@ dependencies {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
 tasks {
     withType<KotlinCompile> {
         compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_1_8)
+            jvmTarget.set(JvmTarget.JVM_17)
         }
     }
 
