d0c53e69957c8d457d8fa6a5e8db8db45653f31f
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2024-07-31 12:51:40 -0500

    Update resources.kt
    Substitute Path to Constants

--- src/main/kotlin/io/codemc/api/resources.kt
+++ src/main/kotlin/io/codemc/api/resources.kt
@@ -10,9 +10,9 @@ const val USER_CONFIG = "user-config"
 
 val RESOURCE_CACHE = mutableMapOf<String, String>()
 private val resources = mapOf(
-    "/templates/jenkins/job-freestyle.xml" to "job-freestyle",
-    "/templates/jenkins/job-maven.xml" to "job-maven",
-    "/templates/jenkins/user-config.xml" to "user-config",
+    "/templates/jenkins/job-freestyle.xml" to JOB_FREESTYLE,
+    "/templates/jenkins/job-maven.xml" to JOB_MAVEN,
+    "/templates/jenkins/user-config.xml" to USER_CONFIG,
 )
 
 suspend fun loadResources() = withContext(Dispatchers.IO) {
