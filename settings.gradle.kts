dcb4ebee5d9c087759b6793676314166ca7d4e84
GitHub Action <action@github.com>
2024-08-24 04:24:04 +0000

    Update Dokka (fa55d9b)

--- settings.gradle.kts
+++
@@ -1,1 +0,0 @@
-rootProject.name = "codemc-api"
--- src/main/kotlin/io/codemc/api/generator.kt
+++
@@ -1,9 +0,0 @@
-@file:JvmName("Generator")
-
-package io.codemc.api
-
-private val pwdChars = ('a'..'z') + ('A'..'Z') + ('0'..'9') + "!@#-_^*".toList()
-
-fun createPassword(size: Int): String = (1..size)
-    .map { pwdChars.random() }
-    .joinToString("")
